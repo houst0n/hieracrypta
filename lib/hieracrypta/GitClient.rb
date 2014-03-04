3ddaa17f3d39c5ed7671608969a8297c06e77d81
Justin Rowles <justin.rowles@bgch.co.uk>
2014-03-04 09:31:14 +0000

    Implemented file fetch by tag

--- lib/hieracrypta/GitClient.rb
+++ lib/hieracrypta/GitClient.rb
@@ -7,10 +7,25 @@ module Hieracrypta
       @rugged=Rugged::Repository.new(repository_location)
     end
 
-    def get_tag(tag, file)
-      "get_tag #{tag} #{file}"
+    def get_tag(tag, file) 
+      begin
+        root = @rugged.rev_parse(tag).tree()
+      rescue Rugged::ReferenceError
+        puts "No such tag refs/tags/#{tag}"
+        return nil
+      end
+      file_parts=file.split('/')
+      for file_part in file_parts do
+        root_hash=root[file_part]
+        if root_hash.nil?
+          return nil
+        end
+        root=Rugged::Object.lookup(@rugged, root_hash[:oid])
+      end
+      
+      root.content
     end
-
+    
     def get_branch(branch, file)
       rugged_branch = Rugged::Branch.lookup(@rugged, branch)
       puts rugged_branch
