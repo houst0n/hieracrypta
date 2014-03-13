4887ae933fbe258090938274277a2c0ac6030821
Justin Rowles <justin.rowles@bgch.co.uk>
2014-03-13 13:39:42 +0000

    Introduce Configuration

--- lib/hieracrypta.rb
+++ lib/hieracrypta.rb
@@ -6,5 +6,6 @@ require 'hieracrypta/Main.rb'
 require 'hieracrypta/Secret.rb'
 require 'hieracrypta/VERSION.rb'
 require 'hieracrypta/WebService.rb'
+require 'hieracrypta/Configuration.rb'
 require 'hieracrypta/Exceptions/Error.rb'
 
---
+++ lib/hieracrypta/Configuration.rb
@@ -0,0 +1,31 @@
+require 'json'
+
+module Hieracrypta
+  class Configuration
+    def initialize(config_file=File.expand_path("config.json", File.dirname(__FILE__)))
+      json_config = File.read(config_file)
+      @config=JSON.parse(json_config)
+    end
+    
+    def get(config_item)
+      path_items=config_item.split('/')
+      result=@config
+      path_items.each { | item |
+        result=result[item]
+        if result.nil?
+          raise NoSuchConfiguration.new(config_item)
+        end
+      }
+      result
+    end
+    
+    def get_admin_keyring_location
+      admin_keyring_location=@config['admin_keyring_location']
+      if !(admin_keyring_location.start_with?('/'))
+        File.expand_path(admin_keyring_location, File.dirname(__FILE__))
+      end
+    end
+  end
+  CONFIGURATION=Configuration.new()
+  attr_reader :CONFIGURATION
+end
